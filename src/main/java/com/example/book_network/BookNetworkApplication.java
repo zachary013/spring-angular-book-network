184053bb7bd21fee948dc8bd39b69687c2f78c62
Zakariae Azn <azarkanzakariae@gmail.com>
2024-10-19 19:32:31 +0100

    User Class Initialization

--- src/main/java/com/example/book_network/BookNetworkApplication.java
+++ src/main/java/com/example/book_network/BookNetworkApplication.java
@@ -2,8 +2,10 @@ package com.example.book_network;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookNetworkApplication {
 
 	public static void main(String[] args) {
