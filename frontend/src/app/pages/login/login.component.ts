29823ad4cf6da68d183ec03f2371638beac6a78a
Zakariae Azn <azarkanzakariae@gmail.com>
2024-11-04 19:48:37 +0100

    Token

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import {AuthenticationRequest} from '../../services/models/authentication-request';
 import {FormsModule} from '@angular/forms';
+import {AuthenticationService} from '../../services/services/authentication.service';
+import {Router} from '@angular/router';
+import {AuthenticationResponse} from '../../services/models/authentication-response';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf,
+    NgForOf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -15,11 +21,33 @@ export class LoginComponent {
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMessage: Array<string> = [];
 
-  login() {
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService,
+    //another serv
+  ) {}
 
+  login() {
+    this.errorMessage = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res:AuthenticationResponse):void => {
+        //todo save token
+        this.router.navigate(['books']);
+      },
+      error: (err): void => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMessage = err.error.validationErrors;
+        } else {
+          this.errorMessage.push(err.error.error);
+        }
+      }
+    })
   }
 
   register() {
-
+    this.router.navigate(['register']);
   }
 }
